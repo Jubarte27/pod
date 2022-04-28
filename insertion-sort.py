988e3a34dfeff2141e1be9e47f1be53aa74bef8e
Matheus Machado <matheusmazar@gmail.com>
2022-04-27 22:08:16 -0300

    Update insertion-sort.py

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,24 +1,33 @@
 import random
 
 
-def insertion_sort(entries):
+def insertion_sort(keys):
     ordered_list = []
-    if len(entries) > 0:
-        ordered_list.append(entries[0])
-        for i in range(1, len(entries)):
-            j = i
-            while j > 0 and entries[i] < ordered_list[j - 1]:
-                j -= 1
-            ordered_list.insert(j, entries[i])
+    for i in range(len(keys)):
+        j = i
+        while j > 0 and keys[i] < ordered_list[j - 1]:
+            j -= 1
+        ordered_list.insert(j, keys[i])
+    return ordered_list
 
+
+def insertion_sort_entries(entries):
+    ordered_list = []
+    for i in range(len(entries)):
+        j = i
+        while j > 0 and entries[i][0] < ordered_list[j - 1][0]:
+            j -= 1
+        ordered_list.insert(j, entries[i])
     return ordered_list
 
 
 def main():
-    entries = [random.randint(0, 100) for i in range(10)]
-    # entries = [4, 12, 4, 10, -1]
+    keys = [random.randint(0, 100) for _ in range(10)]
+    entries = [(k, i) for i, k in enumerate(keys)]
+    print(keys)
+    print(insertion_sort(keys))
     print(entries)
-    print(insertion_sort(entries))
+    print(insertion_sort_entries(entries))
 
 
 main()
